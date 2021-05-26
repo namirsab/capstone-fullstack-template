81aec141cb87130877ed596654517ec7a2e1be92
Namir Sayed-Ahmad <namirsab@gmail.com>
2021-05-26 14:31:47 +0200

    Improve template: server now serves the React app as well. Add proxy

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [state, setState] = useState("");
+
+  useEffect(() => {
+    fetch("/api/hello-world")
+      .then((res) => res.json())
+      .then((data) => setState(data));
+  });
+
   return (
     <div className="App">
-      <h1> Hello React </h1>
+      <h1> {state} </h1>
     </div>
   );
 }
